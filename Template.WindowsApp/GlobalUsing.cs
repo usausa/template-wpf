ddfe88e5a36e31763918370f2275c2905f19936b
usausa <machi.pon@gmail.com>
2022-02-09 18:12:53 +0900

    Update global using

--- Template.WindowsApp/GlobalUsing.cs
+++ Template.WindowsApp/GlobalUsing.cs
@@ -2,6 +2,7 @@
 #pragma warning disable
 global using System.Collections;
 global using System.Collections.Generic;
+global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
@@ -14,4 +15,5 @@ global using System.Windows.Input;
 global using Microsoft.Extensions.Logging;
 
 global using Smart.ComponentModel;
+global using Smart.Navigation;
 global using Smart.Windows.ViewModels;
