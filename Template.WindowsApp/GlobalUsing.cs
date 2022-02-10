22c61cd272575b1233c82be513715902aff9fce4
usausa <machi.pon@gmail.com>
2022-02-10 13:00:36 +0900

    Update global using

--- Template.WindowsApp/GlobalUsing.cs
+++ Template.WindowsApp/GlobalUsing.cs
@@ -8,6 +8,10 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
+global using System.Reactive.Disposables;
+global using System.Reactive.Linq;
+global using System.Reactive.Subjects;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Windows;
@@ -15,11 +19,13 @@ global using System.Windows.Input;
 
 global using Microsoft.Extensions.Logging;
 
+global using Smart;
 global using Smart.Collections.Generic;
 global using Smart.ComponentModel;
 global using Smart.Navigation;
 global using Smart.Navigation.Attributes;
 global using Smart.Navigation.Plugins.Parameter;
 global using Smart.Navigation.Plugins.Scope;
+global using Smart.Text;
 global using Smart.Windows.Messaging;
 global using Smart.Windows.ViewModels;
