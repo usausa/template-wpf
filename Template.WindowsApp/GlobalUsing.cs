ee6a8df89a121d35fbebe254db36a8131a3f863d
usausa <machi.pon@gmail.com>
2022-09-03 12:20:42 +0900

    Update setting

--- Template.WindowsApp/GlobalUsing.cs
+++ Template.WindowsApp/GlobalUsing.cs
@@ -1,20 +1,25 @@
 // ReSharper disable RedundantUsingDirective.Global
 #pragma warning disable
 global using System;
+global using System.Buffers;
 global using System.Collections;
 global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
+global using System.Data;
 global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.IO;
 global using System.Linq;
+global using System.Net;
 global using System.Net.Http;
 global using System.Reactive.Disposables;
 global using System.Reactive.Linq;
 global using System.Reactive.Subjects;
+global using System.Runtime.CompilerServices;
+global using System.Text;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Windows;
