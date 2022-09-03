5cf29461c14fbbd16809000a2b94152fa1a4df20
usausa <machi.pon@gmail.com>
2022-09-03 19:52:19 +0900

    Update

--- Template.WindowsApp/GlobalUsing.cs
+++ Template.WindowsApp/GlobalUsing.cs
@@ -23,7 +23,9 @@ global using System.Text;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Windows;
+global using System.Windows.Data;
 global using System.Windows.Input;
+global using System.Windows.Media;
 
 global using Microsoft.Extensions.Logging;
 
