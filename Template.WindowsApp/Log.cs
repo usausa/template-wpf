db04507e8e6cbef7dc7249265288fe4f6bdbb9f7
usausa <machi.pon@gmail.com>
2024-02-06 13:49:39 +0900

    Update setting

--- Template.WindowsApp/Log.cs
+++ Template.WindowsApp/Log.cs
@@ -1,7 +1,6 @@
 namespace Template.WindowsApp;
 
-#pragma warning disable SYSLIB1006
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Information, Message = "Execute start.")]
     public static partial void InfoExecuteStart(this ILogger logger);
@@ -9,4 +8,3 @@ public static partial class Log
     [LoggerMessage(Level = LogLevel.Information, Message = "Execute end.")]
     public static partial void InfoExecuteEnd(this ILogger logger);
 }
-#pragma warning restore SYSLIB1006
