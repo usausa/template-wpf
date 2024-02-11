45f6c8ad5322598977faba0ad54d49581278088d
usausa <machi.pon@gmail.com>
2024-02-11 14:36:03 +0900

    Update

--- Template.WindowsApp/App.xaml.cs
+++ Template.WindowsApp/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Text.Encodings.Web;
 using System.Text.Json.Serialization;
 using System.Text.Unicode;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -27,26 +28,35 @@ public sealed partial class App
         Current.DispatcherUnhandledException += (_, ea) => HandleException(ea.Exception);
         AppDomain.CurrentDomain.UnhandledException += (_, ea) => HandleException((Exception)ea.ExceptionObject);
 #endif
-        host = Host.CreateDefaultBuilder()
-            .UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration))
-            .UseServiceProviderFactory(new SmartServiceProviderFactory())
-            .ConfigureContainer<ResolverConfig>(ConfigureContainer)
-            .Build();
+        var builder = Host.CreateApplicationBuilder();
+
+        // Logging
+        builder.Logging.ClearProviders();
+        builder.Services.AddSerilog(options =>
+        {
+            options.ReadFrom.Configuration(builder.Configuration);
+        });
+
+        // Container
+        builder.ConfigureContainer(new SmartServiceProviderFactory(), x => ConfigureContainer(builder.Configuration, x));
+
+        host = builder.Build();
     }
 
-    private static void ConfigureContainer(HostBuilderContext context, ResolverConfig config)
+    private static void ConfigureContainer(ConfigurationManager configuration, ResolverConfig config)
     {
         config
             .UseAutoBinding()
             .UseArrayBinding()
             .UseAssignableBinding();
 
-        config.BindConfig<ClientSettings>(context.Configuration.GetSection("Client"));
+        config.BindConfig<ClientSettings>(configuration.GetSection("Client"));
 
         config.BindSingleton<IWindowManager, WindowManager>();
         config.BindSingleton<MainWindow>();
     }
 
+#pragma warning disable IDE0001
     protected override async void OnStartup(StartupEventArgs e)
     {
         await host.StartAsync().ConfigureAwait(false);
@@ -62,6 +72,7 @@ public sealed partial class App
 
         MainWindow = host.Services.GetRequiredService<IWindowManager>().Load();
     }
+#pragma warning restore IDE0001
 
     protected override async void OnExit(ExitEventArgs e)
     {
