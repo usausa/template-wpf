ca2583803044fe25ae143f13ff88ca5bd4e5fd99
usausa <machi.pon@gmail.com>
2025-04-09 13:28:41 +0900

    Update baseline

--- Template.WindowsApp/App.xaml.cs
+++ Template.WindowsApp/App.xaml.cs
@@ -1,30 +1,86 @@
 namespace Template.WindowsApp;
 
+using System.Runtime.InteropServices;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Smart.Windows.Resolver;
+
 using Template.WindowsApp.Views;
 
 public sealed partial class App
 {
+    private readonly IHost host;
+
     private readonly ILogger<App> log;
 
     private readonly IWindowManager windowManager;
 
-    public App(
-        ILogger<App> log,
-        IWindowManager windowManager)
+    //--------------------------------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------------------------------
+
+    public App()
     {
-        this.log = log;
-        this.windowManager = windowManager;
+        InitializeComponent();
+
+        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+        host = CreateHost();
+
+        log = host.Services.GetRequiredService<ILogger<App>>();
+        this.windowManager = host.Services.GetRequiredService<IWindowManager>();
+        ResolveProvider.Default.Provider = host.Services;
+
+        var environment = host.Services.GetRequiredService<IHostEnvironment>();
+        ThreadPool.GetMinThreads(out var workerThreads, out var completionPortThreads);
+
+        log.InfoStartup();
+        log.InfoStartupSettingsRuntime(RuntimeInformation.OSDescription, RuntimeInformation.FrameworkDescription, RuntimeInformation.RuntimeIdentifier);
+        log.InfoStartupSettingsGC(GCSettings.IsServerGC, GCSettings.LatencyMode, GCSettings.LargeObjectHeapCompactionMode);
+        log.InfoStartupSettingsThreadPool(workerThreads, completionPortThreads);
+        log.InfoStartupApplication(environment.ApplicationName, typeof(App).Assembly.GetName().Version);
+        log.InfoStartupEnvironment(environment.EnvironmentName, environment.ContentRootPath);
 
         Current.DispatcherUnhandledException += (_, ea) => HandleException(ea.Exception);
         AppDomain.CurrentDomain.UnhandledException += (_, ea) => HandleException((Exception)ea.ExceptionObject);
-
-        InitializeComponent();
     }
 
-    protected override void OnStartup(StartupEventArgs e)
+    private static IHost CreateHost()
+    {
+        var builder = Host.CreateApplicationBuilder(Environment.GetCommandLineArgs());
+
+        // Log
+        builder.ConfigureLogging();
+        // Components
+        builder.ConfigureComponents();
+
+        return builder.Build();
+    }
+
+    //--------------------------------------------------------------------------------
+    // Lifecycle
+    //--------------------------------------------------------------------------------
+
+    // ReSharper disable once AsyncVoidMethod
+    protected override async void OnStartup(StartupEventArgs e)
     {
         MainWindow = windowManager.Load();
+
+        await host.StartAsync().ConfigureAwait(false);
     }
+
+    // ReSharper disable once AsyncVoidMethod
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await host.StopAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        host.Dispose();
+    }
+
+    //--------------------------------------------------------------------------------
+    // Event
+    //--------------------------------------------------------------------------------
 
     private void HandleException(Exception ex)
     {
--- Template.WindowsApp/ApplicationExtensions.cs
+++ Template.WindowsApp/ApplicationExtensions.cs
@@ -1,6 +1,5 @@
 namespace Template.WindowsApp;
 
-using System.Runtime.InteropServices;
 using System.Text.Encodings.Web;
 using System.Text.Json.Serialization;
 using System.Text.Unicode;
@@ -14,7 +13,6 @@ using Rester;
 using Serilog;
 
 using Smart.Resolver;
-using Smart.Windows.Resolver;
 
 using Template.WindowsApp.Settings;
 using Template.WindowsApp.Views;
@@ -22,24 +20,10 @@ using Template.WindowsApp.Views;
 public static class ApplicationExtensions
 {
     //--------------------------------------------------------------------------------
-    // System
-    //--------------------------------------------------------------------------------
-
-    public static IHostApplicationBuilder ConfigureSystemDefaults(this IHostApplicationBuilder builder)
-    {
-        Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
-        Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
-
-        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
-
-        return builder;
-    }
-
-    //--------------------------------------------------------------------------------
     // Logging
     //--------------------------------------------------------------------------------
 
-    public static IHostApplicationBuilder ConfigureLoggingDefaults(this IHostApplicationBuilder builder)
+    public static IHostApplicationBuilder ConfigureLogging(this IHostApplicationBuilder builder)
     {
         builder.Logging.ClearProviders();
         builder.Services.AddSerilog(options =>
@@ -84,30 +68,5 @@ public static class ApplicationExtensions
 
         config.BindSingleton<IWindowManager, WindowManager>();
         config.BindSingleton<MainWindow>();
-    }
-
-    //--------------------------------------------------------------------------------
-    // Startup
-    //--------------------------------------------------------------------------------
-
-    public static void LogStartupInformation(this IHost app)
-    {
-        var log = app.Services.GetRequiredService<ILogger<Program>>();
-        var environment = app.Services.GetRequiredService<IHostEnvironment>();
-        ThreadPool.GetMinThreads(out var workerThreads, out var completionPortThreads);
-
-        log.InfoStartup();
-        log.InfoStartupSettingsRuntime(RuntimeInformation.OSDescription, RuntimeInformation.FrameworkDescription, RuntimeInformation.RuntimeIdentifier);
-        log.InfoStartupSettingsGC(GCSettings.IsServerGC, GCSettings.LatencyMode, GCSettings.LargeObjectHeapCompactionMode);
-        log.InfoStartupSettingsThreadPool(workerThreads, completionPortThreads);
-        log.InfoStartupApplication(environment.ApplicationName, typeof(Program).Assembly.GetName().Version);
-        log.InfoStartupEnvironment(environment.EnvironmentName, environment.ContentRootPath);
-    }
-
-    public static void RunApplication(this IHost app)
-    {
-        ResolveProvider.Default.Provider = app.Services;
-
-        app.Services.GetRequiredService<App>().Run();
     }
 }
