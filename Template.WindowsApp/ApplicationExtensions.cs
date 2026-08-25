41c68eca8a429151e56a6c0b2de1e636f9f044a1
usausa <machi.pon@gmail.com>
2026-08-25 09:52:17 +0900

    Update

--- Template.WindowsApp/ApplicationExtensions.cs
+++ Template.WindowsApp/ApplicationExtensions.cs
@@ -6,7 +6,6 @@ using System.Text.Unicode;
 
 using BunnyTail.ServiceRegistration;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
