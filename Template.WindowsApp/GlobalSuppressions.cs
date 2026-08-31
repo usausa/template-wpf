69bd59c9872889bd1cf06fe42d172dde4f88b7b6
usausa <machi.pon@gmail.com>
2026-08-31 15:51:09 +0900

    Update

--- Template.WindowsApp/GlobalSuppressions.cs
+++ Template.WindowsApp/GlobalSuppressions.cs
@@ -1,1 +1,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1873:Avoid potentially expensive logging", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
