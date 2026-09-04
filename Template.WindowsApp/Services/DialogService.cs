efae415ccd55ad216099ecf16aca88b2384b5094
usausa <machi.pon@gmail.com>
2026-09-04 10:59:48 +0900

    Update

--- Template.WindowsApp/Services/DialogService.cs
+++ Template.WindowsApp/Services/DialogService.cs
@@ -5,20 +5,29 @@ using Template.WindowsApp.Views.Dialogs;
 // ReSharper disable once ClassNeverInstantiated.Global
 public sealed class DialogService : IDialogService
 {
+    private static Window? Owner => Application.Current.MainWindow;
+
     public bool Confirm(string message) =>
-        MessageBox.Show(Application.Current.MainWindow!, message, "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
+        Show(message, "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
 
     public string? Input(string title, string? initial = null)
     {
         var dialog = new InputDialog
         {
-            Owner = Application.Current.MainWindow,
             Title = title,
             Value = initial ?? string.Empty
         };
+        if (Owner is { } owner)
+        {
+            dialog.Owner = owner;
+        }
+
         return dialog.ShowDialog() == true ? dialog.Value : null;
     }
 
     public void Notify(string message) =>
-        MessageBox.Show(Application.Current.MainWindow!, message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        Show(message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+    private static MessageBoxResult Show(string message, string caption, MessageBoxButton button, MessageBoxImage icon) =>
+        Owner is { } owner ? MessageBox.Show(owner, message, caption, button, icon) : MessageBox.Show(message, caption, button, icon);
 }
